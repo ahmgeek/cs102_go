b1de1651618130f1e198cc961e5b5505120869bb
Ahmad A.Elghany <tolpa1@gmail.com>
2016-11-17 13:15:52 +0200

    Add double linked lists

--- linked_lists.go
+++ linked_lists.go
@@ -1,25 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type node struct {
 	next  *node
-	value int
+	prev *node
+	value float64
 }
 
 func main() {
 	startElement := new(node)
 	scndElement := new(node)
 	thrdElement := new(node)
+	frthElement := new(node)
 
 	startElement.value = 1
 	startElement.next = scndElement
 
-	scndElement.value = 3
+	scndElement.value = 2
 	scndElement.next = thrdElement
+	scndElement.prev = startElement
 
-	thrdElement.value = 5
+	thrdElement.value = 3
 	thrdElement.next = nil
+	thrdElement.prev = scndElement
+
+	for i := startElement; ; i = i.next {
+		fmt.Println("Value is: ", i.value)
+		if i.next == nil {
+			break
+		}
+	}
+
+	fmt.Println("--------------------------------------")
+
+	frthElement.value = 2.5
+	frthElement.next = thrdElement
+	frthElement.prev = scndElement
+
+	thrdElement.prev = frthElement
+	scndElement.next = frthElement
 
 	for i := startElement; ; i = i.next {
 		fmt.Println("Value is: ", i.value)
